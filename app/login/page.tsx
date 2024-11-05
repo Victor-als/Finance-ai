39ea8d95ce70c781d7fe289e62ecf1ad324e4773
Victor-als <victor.ccx814@gmail.com>
2024-11-05 15:17:03 -0300

    feat: add authentication with clerk

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,9 +1,17 @@
 import Image from "next/image";
 import { Button } from "../_components/ui/button";
 import { LogInIcon } from "lucide-react";
+import { SignInButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
 
-const LoginPage = () => {
+const LoginPage = async () => {
+  const { userId } = await auth();
+
+  if (userId) {
+    redirect("/");
+  }
   return ( 
     <div className="grid h-full grid-cols-2">
       <div  className="flex h-full flex-col p-8 justify-center max-w-[550px] mx-auto">
@@ -21,10 +29,15 @@ const LoginPage = () => {
           monitorar suas movimentações, e oferecer insights personalizados, 
           facilitando o controle do seu orçamento.
         </p>
-        <Button variant="outline">
-          <LogInIcon className="mr-2"/>
-          Fazer login ou criar conta
-        </Button>
+
+        <SignInButton>
+            <Button 
+            variant="outline"
+            >
+              <LogInIcon className="mr-2"/>
+              Fazer login ou criar conta
+            </Button>
+        </SignInButton>
       </div>
 
       <div className="relative h-full w-full">
