5e8adfd228c4c45f9f58d65049e3b5d5ca643f87
Victor-als <victor.ccx814@gmail.com>
2024-11-08 14:51:14 -0300

    feat: add navbar component

--- app/subscription/page.tsx
+++ app/subscription/page.tsx
@@ -1,7 +1,12 @@
+import Navbar from "../_components/navbar";
+
 const SubscriptionPage = () => {
-  return ( 
-    <h1>Subscription</h1>
+  return (
+    <>
+      <Navbar />
+      <h1>Subscription</h1>
+    </>
   );
-}
- 
+};
+
 export default SubscriptionPage;
