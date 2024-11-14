6fad02ca5e3b3d5f0090345ef6e6a821224556c2
Victor-als <victor.ccx814@gmail.com>
2024-11-14 15:17:06 -0300

    chore: add image to login screen

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -5,43 +5,40 @@ import { SignInButton } from "@clerk/nextjs";
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 
-
 const LoginPage = async () => {
   const { userId } = await auth();
 
   if (userId) {
     redirect("/");
   }
-  return ( 
+  return (
     <div className="grid h-full grid-cols-2">
-      <div  className="flex h-full flex-col p-8 justify-center max-w-[550px] mx-auto">
-        <Image  
-          src="/logo.svg" 
-          width={173} 
-          height={39} 
-          alt="Finance AI" 
+      <div className="mx-auto flex h-full max-w-[550px] flex-col justify-center p-8">
+        <Image
+          src="/login.png"
+          width={173}
+          height={39}
+          alt="Finance AI"
           className="mb-8"
         />
 
-        <h1 className="text-4xl font-bold mb-3">Bem-vindo</h1>
+        <h1 className="mb-3 text-4xl font-bold">Bem-vindo</h1>
         <p className="mb-8 text-muted-foreground">
-          A Finance AI é uma plataforma de gestão financeira que utiliza IA para 
-          monitorar suas movimentações, e oferecer insights personalizados, 
+          A Finance AI é uma plataforma de gestão financeira que utiliza IA para
+          monitorar suas movimentações, e oferecer insights personalizados,
           facilitando o controle do seu orçamento.
         </p>
 
         <SignInButton>
-            <Button 
-            variant="outline"
-            >
-              <LogInIcon className="mr-2"/>
-              Fazer login ou criar conta
-            </Button>
+          <Button variant="outline">
+            <LogInIcon className="mr-2" />
+            Fazer login ou criar conta
+          </Button>
         </SignInButton>
       </div>
 
       <div className="relative h-full w-full">
-        <Image 
+        <Image
           src="/background-login.png"
           alt="Faça login"
           className="object-cover"
@@ -49,7 +46,7 @@ const LoginPage = async () => {
         />
       </div>
     </div>
-   );
-}
- 
+  );
+};
+
 export default LoginPage;
